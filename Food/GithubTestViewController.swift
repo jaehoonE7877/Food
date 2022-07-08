e8a8e980d03c14b6571c016794fc5bd247fd8c28
jaehoon <sjh22058@gmail.com>
2022-07-08 09:48:52 +0900

    ViewController 코드 추가 및 GithunTestViewController 파일 추가

---
+++ Food/GithubTestViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GithubTestViewController.swift
+//  Food
+//
+//  Created by Seo Jae Hoon on 2022/07/08.
+//
+
+import Foundation
--- Food/ViewController.swift
+++ Food/ViewController.swift
@@ -12,16 +12,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(1)
-        print(2)
-        print(3)
+        print(10)
+        print(20)
+        print(30)
         
         
-        print(4)
-        print(5)
+        print(40)
+        print(50)
         
-        print(6)
-        print(7)
+        print(60)
+        print(70)
         print(8)
     }
 
